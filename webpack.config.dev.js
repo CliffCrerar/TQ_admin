b408ffd2109c32b77ab351df75e17c63668c392d
cliff-cqs <cliffordc@acl.co.za>
2018-07-10 19:17:58 +0200

    updating git ignore

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,82 +7,85 @@ const ManifestPlugin = require('clean-webpack-plugin');
 //const HtmlWebpackPlugin = require('Ht')
 
 module.exports = {
-  mode: 'development',
-  entry: ['./src/index.js', './src/devCode.js'],
-  target: 'node',
-  devtool: 'inline-source-map',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader'
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {}
-          }
-        ]
-      },
-      {
-        test: /\.(html)$/,
-        use: {
-          loader: 'html-loader',
-          options: {
-            attrs: [':data-src']
-          }
-        }
-      },
-      {
-        test: /\.exec\.js$/,
-        use: ['script-loader']
-      }
-      /*
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192
+    mode: 'development',
+    entry: ['./src/index.js', './src/devCode.js'],
+    target: 'node',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
+            },
+            {
+                test: /\.(html)$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        attrs: [':data-src']
+                    }
+                }
+            },
+            {
+                test: /\.exec\.js$/,
+                use: ['script-loader']
             }
-          }
+            /*
+            {
+              test: /\.(png|jpg|gif)$/,
+              use: [
+                {
+                  loader: 'url-loader',
+                  options: {
+                    limit: 8192
+                  }
+                }
+              ]
+            }
+            */
         ]
-      }
-      */
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            ADDRESS: JSON.stringify('http://172.16.0.104:8010/'),
+            ADDRESSFS: JSON.stringify('http://172.16.0.104:8015')
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': "jquery'",
+            'window.$': 'jquery',
+            Popper: ['popper.js', 'default']
+        }),
+        new HTMLWebpackPlugin({
+            title: 'TQ Admin Console',
+            favicon: './src/img/favicon.svg'
+        }),
+        new CleanWebpackPlugin(['dist']),
+        new ManifestPlugin(['Manifest'])
     ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      ADDRESS: JSON.stringify('http://172.16.0.104:8010/'),
-      ADDRESSFS: JSON.stringify('http://172.16.0.104:8015')
-    }),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': "jquery'",
-      'window.$': 'jquery',
-      Popper: ['popper.js', 'default']
-    }),
-    new HTMLWebpackPlugin({
-      title: 'TQ Admin Console',
-      favicon: './src/img/favicon.svg'
-    }),
-    new CleanWebpackPlugin(['dist']),
-    new ManifestPlugin(['Manifest'])
-  ]
 };
